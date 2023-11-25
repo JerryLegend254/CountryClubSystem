76d15a32a4b50f0ae71daf1d777212a6167a531d
JerryLegend254 <jerryokuto713@gmail.com>
2023-11-25 23:57:41 +0300

    Routes and controllers refactor

--- CountryClubBE/src/routes/sportsplan/sportsplan.controller.js
+++ CountryClubBE/src/routes/sportsplan/sportsplan.controller.js
@@ -1,65 +1,64 @@
-const { addDoc, collection, query, getDocs, getDoc, doc } = require("firebase/firestore");
+const {
+  addDoc,
+  collection,
+  query,
+  getDocs,
+  getDoc,
+  doc,
+} = require("firebase/firestore");
 const { db } = require("../../firebase");
 
 async function httpAddSportsplan(req, res) {
-    try {
-        const data = req.body
-        await addDoc(collection(db, "sportsplan"), data);
-        return res.status(201).json({ msg: "Added sportsplan" })
-    } catch (err) {
-
-        return res.status(400).json({ msg: err })
-    }
-
+  try {
+    const data = req.body;
+    await addDoc(collection(db, "sportsplan"), data);
+    return res.status(201).json({ msg: "Added sportsplan" });
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
 }
 
 async function httpGetAllSportsplan(req, res) {
-    try {
-        const q = query(collection(db, "sportsplan"));
-        const sportsplans = []
+  try {
+    const q = query(collection(db, "sportsplan"));
+    const sportsplans = [];
 
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-            sportsplans.push({id: doc.id, ...doc.data()})
-        });
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      sportsplans.push({ id: doc.id, ...doc.data() });
+    });
 
-        return res.status(200).json(sportsplans)
-    } catch (err) {
-
-        return res.status(400).json({ msg: err })
-    }
-
+    return res.status(200).json(sportsplans);
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
 }
-
 
 async function httpGetOneSportsplan(req, res) {
-    try {
-        console.log(req)
-        const sportsplanId = req.params.id
-        const docRef = doc(db, "sportsplan", sportsplanId);
-        const docSnap = await getDoc(docRef);
-        
-        if (docSnap.exists()) {
-          console.log("Document data:", docSnap.data());
-          const data = docSnap.data()
-          console.log("Found Document")
-          return res.status(200).json(data)
-        } else {
-          // docSnap.data() will be undefined in this case
-          return res.status(404).json({ msg: "Document not found" })
-        }
+  try {
+    console.log(req);
+    const sportsplanId = req.params.id;
+    const docRef = doc(db, "sportsplan", sportsplanId);
+    const docSnap = await getDoc(docRef);
 
-        
-    } catch (err) {
-
-        return res.status(400).json({ msg: err })
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      const data = docSnap.data();
+      console.log("Found Document");
+      return res.status(200).json(data);
+    } else {
+      // docSnap.data() will be undefined in this case
+      return res.status(404).json({ msg: "Document not found" });
     }
-
+  } catch (err) {
+    return res.status(400).json({ msg: err });
+  }
 }
-
 
 module.exports = {
-    httpAddSportsplan, httpGetAllSportsplan, httpGetOneSportsplan
-}
+  httpAddSportsplan,
+  httpGetAllSportsplan,
+  httpGetOneSportsplan,
+};
