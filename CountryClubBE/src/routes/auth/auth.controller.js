215712e3a22b27a2bd23474bab52745722b2f186
JerryLegend254 <jerryokuto713@gmail.com>
2023-11-29 11:57:22 +0300

    Code clean up and removing unnecessary code

--- CountryClubBE/src/routes/auth/auth.controller.js
+++ CountryClubBE/src/routes/auth/auth.controller.js
@@ -19,64 +19,11 @@ async function httpEmailSignUp(req, res) {
       disabled: false,
     });
 
-    // const additionalClaims = {
-    //   email: userRecord.email,
-    //   displayName: userRecord.displayName,
-    //   photoURL: userRecord.photoURL,
-    // };
-    // const customToken = await adminAuth.createCustomToken(
-    //   userRecord.uid,
-    //   additionalClaims
-    // );
-
-    // Assign a role to the user
-    const role = "User"; // Set the default role or determine it based on your logic
-    await setDoc(doc(db, "userRoles", userRecord.uid), { role });
-    console.log(userRecord);
-
-    // mEMBERS
-    await setDoc(doc(db, "members", userRecord.uid), {
-      username: userRecord.displayName,
-      email: userRecord.email,
-    });
     return res.status(201).json({ msg: "User created successfully" });
   } catch (err) {
     return res.status(400).json({ error: err.message });
   }
 }
-
-// async function httpGetAllUsers(req, res) {
-//   const members = [];
-
-//   function listAllUsers(nextPageToken) {
-//     // List batch of users, 1000 at a time.
-//     adminAuth
-//       .listUsers(1000, nextPageToken)
-//       .then((listUsersResult) => {
-//         listUsersResult.users.forEach((userRecord) => {
-//           // console.log("user", userRecord.toJSON());
-//           members.push(userRecord.toJSON());
-//         });
-//         if (listUsersResult.pageToken) {
-//           // List next batch of users.
-//           listAllUsers(listUsersResult.pageToken);
-//         }
-//         // console.log(members);
-//         return members
-//       })
-//       .catch((error) => {
-//         console.log("Error listing users:", error);
-//         return error;
-//       });
-//   }
-//   try {
-//     const membersList = listAllUsers();
-//     return await res.status(200).json(membersList);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ error: err });
-//   }
-// }
 
 
 async function httpGetAllUsers(req, res) {
@@ -88,9 +35,16 @@ async function httpGetAllUsers(req, res) {
       const listUsersResult = await adminAuth.listUsers(1000, nextPageToken);
 
       listUsersResult.users.forEach((userRecord) => {
-
-        const {uid, displayName, email, emailVerified, disabled, photoURL} = userRecord.toJSON();
-        members.push({uid, disabled, name: displayName, email, emailVerified, photoURL})
+        const { uid, displayName, email, emailVerified, disabled, photoURL } =
+          userRecord.toJSON();
+        members.push({
+          uid,
+          disabled,
+          name: displayName,
+          email,
+          emailVerified,
+          photoURL,
+        });
       });
 
       if (listUsersResult.pageToken) {
@@ -115,50 +69,6 @@ async function httpGetAllUsers(req, res) {
       .json({ error: err.message || "Internal Server Error" });
   }
 }
-
-
-// async function httpEmailSignIn(req, res) {
-//   const { email, password } = req.body;
-//   signInWithEmailAndPassword(clientAuth, email, password)
-//     .then(async (userCredential) => {
-//       const user = userCredential.user;
-//       // Set session cookie
-//       const idToken = await userCredential.user.getIdToken();
-//       res.cookie("session", idToken, {
-//         httpOnly: true,
-//       });
-
-//       console.log(res.session)
-//       return res.status(200).json({ msg: "Signed In", user });
-//     })
-//     .catch((error) => {
-//       const errorMessage = error.message;
-//       return res.status(404).json({ error: errorMessage });
-//     });
-// }
-
-// async function httpEmailSignIn(req, res) {
-//   const { email, password } = req.body;
-
-//   try {
-//     const userCredential = await signInWithEmailAndPassword(
-//       clientAuth,
-//       email,
-//       password
-//     );
-//     const user = userCredential.user;
-
-//     // Set session cookie
-//     const idToken = await user.getIdToken();
-
-//     req.session.token = idToken;
-//     console.log(req.session.token);
-//     res.status(200).json({ msg: "Signed In", user });
-//   } catch (error) {
-//     const errorMessage = error.message;
-//     res.status(404).json({ error: errorMessage });
-//   }
-// }
 
 module.exports = {
   httpEmailSignUp,
--- CountryClubFE/src/components/auth/protected-route.jsx
+++ CountryClubFE/src/components/auth/protected-route.jsx
@@ -10,32 +10,6 @@ function ProtectedRoute({ children }) {
   const { isAuthenticated, user } = useAuth();
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (!isAuthenticated) navigate('/login');
-  //   if (user) {
-  //     if (user.email !== 'admin@gmail.com') navigate('/user-index/user-payments');
-  //   }
-  // }, [isAuthenticated, navigate, user]);
-
-  // useEffect(() => {
-  //   if (!isAuthenticated) {
-  //     navigate('/login');
-  //   } else if (user) {
-  //     if (user.email === 'admin@gmail.com') {
-  //       // Admin can access all routes
-  //       return;
-  //     }
-  //     // Basic user can access user-index/user-payments and user-index/add-plan
-  //     const allowedRoutes = [
-  //       '/user-index/user-payments',
-  //       '/user-index/add-plan',
-  //       '/user-index/profile',
-  //     ];
-  //     if (!allowedRoutes.includes(window.location.pathname)) {
-  //       navigate('/user-index/user-payments');
-  //     }
-  //   }
-  // }, [isAuthenticated, navigate, user]);
   
   useEffect(() => {
     if (!isAuthenticated) {
